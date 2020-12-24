e695e4dd28f94d821bde7e76808189d6803545a2
juan camilo huertas quiroga <juankamilohuertasquiroga@gmail.com>
2020-12-24 15:13:20 -0500

    segundo cambio de imagenes

--- app.js
+++ app.js
@@ -1,18 +1,54 @@
 let imagenes = [
-"https://www.psicoactiva.com/wp-content/uploads/blog/2016/03/frases-amor3-1.jpg",
-"https://www.ecestaticos.com/image/clipping/f07ddc1be5662925804003737e17843c/frases-de-amor-bonitas-y-originales-para-enamorar-y-conquistar.jpg",  
-"https://www.ecured.cu/images/thumb/f/ff/Amor-adentro.jpg/260px-Amor-adentro.jpg"
+"https://i.linio.com/p/ceaeff022357316985c57fa3dd796fc7-product.jpg",
+"https://images-na.ssl-images-amazon.com/images/I/81YM1unmEvL._AC_SX569_.jpg",  
+"https://http2.mlstatic.com/D_NQ_NP_954204-MCO27817388679_072018-O.jpg"
 ]
 
-let adelante = document.getElementById("adelante");
+let previsualisar = document.getElementById("prev");
 let atras = document.getElementById("atras");
-let prev =(img)=>{
-    img = document.getElementById("img");
-    if(img.src=imagenes[0]){
-        img.src=imagenes[1];
-    }else{
+let img = document.getElementById("imagen");
+var conteo = 0;
+var prev =()=>{
+    conteo +=1;
+    switch(conteo){
+    case 1:
         img.src=imagenes[0];
+        break;
+    case 2:
+        img.src=imagenes[1];
+        break;
+    case 3:
+        img.src=imagenes[2];
+        if(conteo = 3){
+            
+        conteo = conte;
+        
+        }
+           
+        break;
     }
-    /* nuevo codigo */
+    console.log(conteo)
 }
-adelante.addEventListener("click", prev);
+previsualisar.addEventListener("click", prev);
+let conte = 0;
+var retro =()=>{
+    conte +=1;
+    console.log(conte)
+    switch(conte){
+    case 1:
+        img.src=imagenes[2];
+        break;
+    case 2:
+        img.src=imagenes[1];
+        break;
+    case 3:
+        img.src=imagenes[0];
+        if(conte >= 3){
+          conte = conteo;
+            
+        } 
+        break;
+    }
+    
+}
+atras.addEventListener("click", retro);
